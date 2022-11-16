b8d7e17c355599173ab6b5995b6469f16cc4a6ec
leonardo padovani <lzardopadovani@gmail.com>
2022-11-16 15:54:03 -0300

    rotas

--- src/test/java/routes/BaseRoutes.java
+++ src/test/java/routes/BaseRoutes.java
@@ -1,7 +1,12 @@
 package routes;
 
 import config.Configuracoes;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.Before;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -9,10 +14,16 @@ import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 public class BaseRoutes {
 
+
     public void setUp(){
         Configuracoes configuracoes = ConfigFactory.create(Configuracoes.class);
         baseURI = configuracoes.baseURI();
-        basePath = configuracoes.basePath();
+        //basePath = configuracoes.basePath();
+
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setContentType(ContentType.JSON);
+        requestSpecification = requestSpecBuilder.build();
+        enableLoggingOfRequestAndResponseIfValidationFails();
 
 
     }
--- src/test/java/routes/Logar.java
+++ src/test/java/routes/Logar.java
@@ -1,12 +1,48 @@
 package routes;
 
+import factories.UsuarioDataFactory;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.Usuario;
+
+import java.io.IOException;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
-public class Logar {
+public class Logar extends BaseRoutes{
 
+    @Test
+    public void postLogar() throws IOException {
+        this.setUp();
+        String teste =
+            given()
+                .body(UsuarioDataFactory.criarUsuarioValido())
+            .when()
+                .post("/signin")
+            .then()
+                .log()
+                    .all()
+                .extract()
+                    .body().asString();
 
+        System.out.println(teste);
+    }
+
+    @Test
+    public void postLogarSemSenha() throws IOException {
+        this.setUp();
+            given()
+                .body(UsuarioDataFactory.criarUsuarioSemSenha())
+            .when()
+                .post("/signin")
+            .then()
+                .statusCode(401)
+                .log()
+                    .all();
+
+    }
 
 }
